6e43328620dc0113ed8741e6e9d1ad934ba7d20d
Audrey Schmidt <audsch@outlook.com>
2022-01-18 14:30:22 -0600

    have 'GET' method to view all comments for a particular video, added 'POST' method to add a new commen tto a video

--- drf_jwt_backend/comments/models.py
+++ drf_jwt_backend/comments/models.py
@@ -9,8 +9,13 @@ class Comment(models.Model):
     likes = models.IntegerField(null=True)
     dislikes = models.IntegerField(null=True)
     
+    def __str__(self):
+        return 'comment' + str(self.user) + str(self.video_id)
     
 class Reply(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=False)
     text = models.CharField(max_length=250, null=False)
+    
+    def __str__(self):
+        return 'reply' + str(self.user) + str(self.comment)
--- drf_jwt_backend/comments/urls.py
+++ drf_jwt_backend/comments/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from comments import views
 
 urlpatterns = [
+    path('all/<str:video_id>/', views.get_all_comments),
+    path('addcomment/', views.user_comments)
 ]
