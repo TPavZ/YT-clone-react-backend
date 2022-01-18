60e3d477a633c84ddae15b769c7acc3e0a21a7f5
Audrey Schmidt <audsch@outlook.com>
2022-01-18 11:22:15 -0600

    changed local_settings to connect to database

--- drf_jwt_backend/drf_jwt_backend/local_settings.py
+++ drf_jwt_backend/drf_jwt_backend/local_settings.py
@@ -9,7 +9,7 @@ DATABASES = {
         'ENGINE': 'mysql.connector.django',
         'NAME': 'drf_jwt_database',
         'USER': 'root',
-        'PASSWORD': 'YOUR MYSQL PASSWORD HERE',
+        'PASSWORD': 'Password1234!',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
