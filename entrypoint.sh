6405f78a969b82332c1626f4fd6f7e8fb6d44371
Audrey Schmidt <audsch@outlook.com>
2022-02-03 14:28:02 -0600

    chore: added entrypoint.sh with operating commands for docker-compose.yml

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+set -e #exit for errors
+
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_PASSWORD=admin
+export DJANGO_SUPERUSER_EMAIL=admin@gmail.com
+
+python ./drf_jwt_backend/manage.py migrate
+
+python ./drf_jwt_backend/manage.py createsuperuser --noinput
+
+python ./drf_jwt_backend/manage.py runserver 0.0.0.0:8000
